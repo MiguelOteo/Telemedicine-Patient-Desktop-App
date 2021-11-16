4c6982fa49bf91681dcae0d3c601760fb3cef44d
Miguel Oteo <47748362+MiguelOteo@users.noreply.github.com>
2021-11-16 21:03:17 +0100

    CheckBox

--- src/addPatientsPopUp/addPatientController.java
+++ src/addPatientsPopUp/addPatientController.java
@@ -14,24 +14,33 @@ import java.util.ResourceBundle;
 
 import com.google.gson.Gson;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXTreeTableColumn;
 import com.jfoenix.controls.JFXTreeTableView;
 import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.cells.editors.base.JFXTreeTableCell;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 
 import communication.AccountObjectCommunication;
 import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableCell;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableColumn.CellDataFeatures;
+import javafx.scene.control.cell.CheckBoxTreeTableCell;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.util.Callback;
@@ -119,8 +128,8 @@ public class addPatientController implements Initializable {
 	private void setList() {
 		
 		JFXTreeTableColumn<PatientTreeObject, String> patientName = new JFXTreeTableColumn<>("Patient name");
-		patientName.setPrefWidth(180);
-		patientName.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<PatientTreeObject,String>, ObservableValue<String>>() {
+		patientName.setPrefWidth(150);
+		patientName.setCellValueFactory(new Callback<JFXTreeTableColumn.CellDataFeatures<PatientTreeObject,String>, ObservableValue<String>>() {
 			@Override
 			public ObservableValue<String> call(CellDataFeatures<PatientTreeObject, String> param) {
 				return param.getValue().getValue().patientName;
@@ -129,8 +138,8 @@ public class addPatientController implements Initializable {
 		patientName.setResizable(false);
 		
 		JFXTreeTableColumn<PatientTreeObject, String> patientIdNumber = new JFXTreeTableColumn<>("Patient ID number");
-		patientIdNumber.setPrefWidth(180);
-		patientIdNumber.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<PatientTreeObject,String>, ObservableValue<String>>() {
+		patientIdNumber.setPrefWidth(150);
+		patientIdNumber.setCellValueFactory(new Callback<JFXTreeTableColumn.CellDataFeatures<PatientTreeObject,String>, ObservableValue<String>>() {
 			@Override
 			public ObservableValue<String> call(CellDataFeatures<PatientTreeObject, String> param) {
 				return param.getValue().getValue().patientIdNumber;
@@ -138,13 +147,26 @@ public class addPatientController implements Initializable {
 		});
 		patientIdNumber.setResizable(false);
 		
+		JFXTreeTableColumn<PatientTreeObject, Boolean> selectedPatients = new JFXTreeTableColumn<>("Patient Selected");
+		selectedPatients.setPrefWidth(150);
+		selectedPatients.setCellFactory(new Callback<TreeTableColumn<PatientTreeObject ,Boolean>,TreeTableCell<PatientTreeObject,Boolean>>() {
+		    @Override 
+		    public TreeTableCell<PatientTreeObject, Boolean> call( TreeTableColumn<PatientTreeObject,Boolean> param) {
+		        CheckBoxTreeTableCell<PatientTreeObject,Boolean> cell = new CheckBoxTreeTableCell<PatientTreeObject,Boolean>();
+		        cell.setAlignment(Pos.CENTER);
+		        return cell;
+		    }
+		});
+		selectedPatients.setResizable(false);
+		
+		
 		for (Patient patient: patientsList) {
-				patientsObjects.add(new PatientTreeObject(patient.getName(), patient.getPatientIdNumber()));
+				patientsObjects.add(new PatientTreeObject(patient.getName(), patient.getPatientIdNumber(), false));
 		}
 		
 		TreeItem<PatientTreeObject> root = new RecursiveTreeItem<PatientTreeObject>(patientsObjects, RecursiveTreeObject::getChildren);
 		PatientTreeView.setPlaceholder(new Label("No patients found"));
-		PatientTreeView.getColumns().setAll(patientName, patientIdNumber);
+		PatientTreeView.getColumns().setAll(patientName, patientIdNumber, selectedPatients);
 		PatientTreeView.setRoot(root);
 		PatientTreeView.setShowRoot(false);
 	}
@@ -154,9 +176,11 @@ class PatientTreeObject extends RecursiveTreeObject<PatientTreeObject> {
 	
 	StringProperty patientName;
 	StringProperty patientIdNumber;
+	BooleanProperty selectedPatient;
 	
-	public PatientTreeObject(String patientName, String patientIdNumber) {
+	public PatientTreeObject(String patientName, String patientIdNumber, boolean selected) {
 		this.patientName = new SimpleStringProperty(patientName);
 		this.patientIdNumber = new SimpleStringProperty(patientIdNumber);
+		this.selectedPatient = new SimpleBooleanProperty(selected);	
 	}
 }
