18638b5d139b97c699c74e15269c8ceede15fa66
MiguelOteo <miguel_oteo@outlook.com>
2021-11-25 12:48:40 +0100

    bitalino lib update

--- src/BITalino/DeviceDiscoverer.java
+++ src/BITalino/DeviceDiscoverer.java
@@ -10,59 +10,50 @@ import javax.bluetooth.ServiceRecord;
 
 public class DeviceDiscoverer implements DiscoveryListener {
 
+	public Vector<RemoteDevice> remoteDevices = new Vector<RemoteDevice>();
+	DiscoveryAgent discoveryAgent;
+	public String deviceName;
+	String inqStatus = null;
 
-    public Vector<RemoteDevice> remoteDevices = new Vector<RemoteDevice>();
-    DiscoveryAgent discoveryAgent;
-    public String deviceName;
-    String inqStatus = null;
+	public DeviceDiscoverer() {
+		try {
+			LocalDevice localDevice = LocalDevice.getLocalDevice();
+			System.err.println(LocalDevice.getLocalDevice());
+			discoveryAgent = localDevice.getDiscoveryAgent();
+			discoveryAgent.startInquiry(DiscoveryAgent.GIAC, this);
 
-    public DeviceDiscoverer() {
-        try {
-            LocalDevice localDevice = LocalDevice.getLocalDevice();
-            System.err.println(LocalDevice.getLocalDevice());
-            discoveryAgent = localDevice.getDiscoveryAgent();
-            discoveryAgent.startInquiry(DiscoveryAgent.GIAC, this);            
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
+	public void deviceDiscovered(RemoteDevice remoteDevice, DeviceClass cod) {
 
-    public void deviceDiscovered(RemoteDevice remoteDevice, DeviceClass cod) {
-        
-    	try
-    	{
-           deviceName=remoteDevice.getFriendlyName(false); //Records devices names
-           if (deviceName.equalsIgnoreCase("bitalino")) 
-           {
-	           remoteDevices.addElement(remoteDevice);
-           }
-          
-        } 
-    	catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
+		try {
+			deviceName = remoteDevice.getFriendlyName(false); // Records devices names
+			if (deviceName.equalsIgnoreCase("bitalino")) {
+				remoteDevices.addElement(remoteDevice);
+			}
 
-    public void inquiryCompleted(int discType) 
-    {
-    
-	    if (discType == DiscoveryListener.INQUIRY_COMPLETED) 
-	    {
-	        inqStatus = "Scan completed.";
-	    }
-	    else if (discType == DiscoveryListener.INQUIRY_TERMINATED) 
-	    {
-	        inqStatus = "Scan terminated.";
-	    }
-	    else if (discType == DiscoveryListener.INQUIRY_ERROR) 
-	    {
-	        inqStatus = "Scan with errors.";
-	    }
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void servicesDiscovered(int transID, ServiceRecord[] servRecord){}
+	public void inquiryCompleted(int discType) {
 
-    public void serviceSearchCompleted(int transID, int respCode) {}
+		if (discType == DiscoveryListener.INQUIRY_COMPLETED) {
+			inqStatus = "Scan completed.";
+		} else if (discType == DiscoveryListener.INQUIRY_TERMINATED) {
+			inqStatus = "Scan terminated.";
+		} else if (discType == DiscoveryListener.INQUIRY_ERROR) {
+			inqStatus = "Scan with errors.";
+		}
+	}
+
+	public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
+	}
+
+	public void serviceSearchCompleted(int transID, int respCode) {
+	}
 }
