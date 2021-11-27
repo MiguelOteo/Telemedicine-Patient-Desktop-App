913230693363de5ed4e14b13b73f94c6d565a0c6
MiguelOteo <miguel_oteo@outlook.com>
2021-11-27 19:44:14 +0100

    Create bitalinoPackage.java

---
+++ src/models/bitalinoPackage.java
@@ -0,0 +1,35 @@
+package models;
+
+import java.sql.Date;
+
+public class bitalinoPackage {
+
+	private int bitalinoPackageId;
+	
+	private int patientId;
+	private int recordFreq;
+	private Date recordsDate;
+	private String recordsData;
+
+	public bitalinoPackage() {}
+
+	public int getBitalinoPackageId() {return bitalinoPackageId;}
+
+	public void setBitalinoPackageId(int bitalinoPackageId) {this.bitalinoPackageId = bitalinoPackageId;}
+
+	public int getPatientId() {return patientId;}
+
+	public void setPatientId(int patientId) {this.patientId = patientId;}
+
+	public int getRecordFreq() {return recordFreq;}
+
+	public void setRecordFreq(int recordFreq) {this.recordFreq = recordFreq;}
+
+	public Date getRecordsDate() {return recordsDate;}
+
+	public void setRecordsDate(Date recordsDate) {this.recordsDate = recordsDate;}
+
+	public String getRecordsData() {return recordsData;}
+
+	public void setRecordsData(String recordsData) {this.recordsData = recordsData;}
+}
