45036573c7e50edef2c85b0ffa974173827eeb61
bermejo4 <minijai72@gmail.com>
2021-11-27 14:46:50 +0100

    Bitalino connection changes

--- src/bitalinoConnectionPane/BitalinoConnection.java
+++ src/bitalinoConnectionPane/BitalinoConnection.java
@@ -1,5 +1,6 @@
 package bitalinoConnectionPane;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
 import javax.bluetooth.RemoteDevice;
@@ -9,9 +10,37 @@ import BITalino.DeviceDiscoverer;
 
 public class BitalinoConnection {
 	
-	public void getBitalinosMACs() throws InterruptedException{
-		Vector<RemoteDevice> availableDevices = new Vector<RemoteDevice>();
-		availableDevices=BITalino.findDevices();
-		
+	public ArrayList<String> getBitalinosMACs() throws InterruptedException{
+		Vector<RemoteDevice> availableDevices = new Vector<RemoteDevice>();		
+    	ArrayList<String> MACsList = new ArrayList<String>();
+    	String tmp="";
+    	String tmp2="";
+		availableDevices=BITalino.findDevices();;
+		System.out.println(availableDevices);
+		 for(int i=0; i<availableDevices.size(); i++){
+	            tmp=availableDevices.elementAt(i).toString();
+	            for(int j=0; j<tmp.length(); j++) {
+	            	tmp2=tmp2+tmp.charAt(j);
+	            	if(j%2!=0) {
+	            		if(j!=(tmp.length()-1)) {
+	            			tmp2=tmp2+":";
+	            		}
+	            	}
+	            }
+	            MACsList.add(tmp2);
+	        }
+		 return MACsList;
+	}
+	
+	public String getBitalinoMacFromAvailableBitalinosList(int num, ArrayList<String> MACsList) {
+		String mac="error";
+		 if(num>=MACsList.size()) {
+			 mac = "Error";
+		 }else {
+			 for(int i=0; i<MACsList.size(); i++){
+				 mac= MACsList.get(num);
+			 } 
+		 }
+		 return mac;
 	}
 }
