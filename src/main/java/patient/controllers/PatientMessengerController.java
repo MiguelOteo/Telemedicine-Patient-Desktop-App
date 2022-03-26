85c76e41294d0411ff2ca868f5276c37bbb07bc1
MiguelOteo <miguel_oteo@outlook.com>
2022-03-26 20:24:49 +0100

    Update PatientMessengerController.java

--- src/main/java/patient/controllers/PatientMessengerController.java
+++ src/main/java/patient/controllers/PatientMessengerController.java
@@ -1,13 +1,17 @@
 package patient.controllers;
 
 import java.net.URL;
+
 import java.util.ResourceBundle;
+
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
 
 public class PatientMessengerController implements Initializable {
 
